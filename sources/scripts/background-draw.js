c5ee6a0100808c294c70694ece41b0ffb55868c1
Caleb Hefty <caleb.hefty@gmail.com>
2022-07-09 13:36:55 -0500

    Finished basic version of background.

--- sources/scripts/background-draw.js
+++ sources/scripts/background-draw.js
@@ -1,14 +1,15 @@
-PIX_PER_AVG_TRI_HEIGHT = 95;
+const AVG_TRI_DIM = 140;
 
+const PRIMARY_COLOR = "black";
+const BASE_COLOR = "white";
 function CoordSet(xCoord, yCoord){
     this.x = xCoord;
     this.y = yCoord;
 }
 
-
 var backElement = document.getElementById("background"),
     two = new Two({ 
-        fullscreen: false
+        fullscreen: true
     });
  
 two.appendTo(backElement);
@@ -20,51 +21,42 @@ function generateGradient(triangle){
     let gradientStartIndex = Math.random() * 3;
 
     if(gradientStartIndex < 1){ 
-        let gradientColor = two.makeLinearGradient(triangle.vertices[0].x,triangle.vertices[0].y,triangle.vertices[1].x,triangle.vertices[1].y,new Two.Stop(0,"purple",0.4),new Two.Stop(1,"white",1));
+        let gradientColor = two.makeLinearGradient(triangle.vertices[0].x,triangle.vertices[0].y,triangle.vertices[1].x,triangle.vertices[1].y,new Two.Stop(0,PRIMARY_COLOR,0.4),new Two.Stop(1,BASE_COLOR,1));
         return gradientColor;
     }else if(gradientStartIndex < 2){
-        let gradientColor = two.makeLinearGradient(triangle.vertices[1].x,triangle.vertices[1].y,triangle.vertices[2].x,triangle.vertices[2].y,new Two.Stop(0,"purple",0.4),new Two.Stop(1,"white",1));
+        let gradientColor = two.makeLinearGradient(triangle.vertices[1].x,triangle.vertices[1].y,triangle.vertices[2].x,triangle.vertices[2].y,new Two.Stop(0,PRIMARY_COLOR,0.4),new Two.Stop(1,BASE_COLOR,1));
         return gradientColor;
     }else{
-        let gradientColor = two.makeLinearGradient(triangle.vertices[2].x,triangle.vertices[2].y,triangle.vertices[0].x,triangle.vertices[0].y,new Two.Stop(0,"purple",0.4),new Two.Stop(1,"white",1));
+        let gradientColor = two.makeLinearGradient(triangle.vertices[2].x,triangle.vertices[2].y,triangle.vertices[0].x,triangle.vertices[0].y,new Two.Stop(0,PRIMARY_COLOR,0.4),new Two.Stop(1,BASE_COLOR,1));
         return gradientColor;
     }
 }
 
-let height = two.height;
-let width  = two.width;
 
-let numAcross = height/PIX_PER_AVG_TRI_HEIGHT;
-let numDown = width/PIX_PER_AVG_TRI_HEIGHT;
+
+let numAcross = two.width/AVG_TRI_DIM + 2;
+let numDown =  two.height/AVG_TRI_DIM + 2;
+
+
 
 let offsetArray = new Array();
-//let YoffsetArray = new Array();
 for(let i = 0; i < numAcross; i++){
     offsetArray[i] = [];
     for(let j = 0; j < numDown; j++){
-        offsetArray[i][j] = new CoordSet(i * PIX_PER_AVG_TRI_HEIGHT + Math.random() * 90, j * PIX_PER_AVG_TRI_HEIGHT + Math.random() * 90);
+        offsetArray[i][j] = new CoordSet((i) * AVG_TRI_DIM + Math.random() * 90, (j) * AVG_TRI_DIM + Math.random() * 90);
     }
 }
-
-for(let i = 0; i < numAcross; i++){
-    for(let j = 0; j < numDown; j++){
- //       let dot = two.makeCircle(offsetArray[i][j].x ,offsetArray[i][j].y,3,5);
-   //     dot.fill = "#ffffff";
-    }
-}
-
-let gradientColor = two.makeLinearGradient(1,1,400,400,new Two.Stop(0,"purple",1),new Two.Stop(1,"white",1));
 
 for(let i = 0; i < numAcross-1; i++){
     for(let j = 0; j < numDown-1; j++){
-        let triangle = two.makePath(offsetArray[i][j].x,offsetArray[i][j].y,offsetArray[i+1][j].x,offsetArray[i+1][j].y, offsetArray[i][j+1].x, offsetArray[i][j+1].y, true);
+        let triangle = two.makePath(offsetArray[i][j].x - AVG_TRI_DIM,offsetArray[i][j].y - AVG_TRI_DIM,offsetArray[i+1][j].x - AVG_TRI_DIM, offsetArray[i+1][j].y - AVG_TRI_DIM, offsetArray[i][j+1].x - AVG_TRI_DIM, offsetArray[i][j+1].y - AVG_TRI_DIM, true);
         triangle.noStroke().fill = generateGradient(triangle);
     }
 }
 
 for(let i = 1; i < numAcross; i++){
     for(let j = 0; j < numDown-1; j++){
-        let triangle = two.makePath(offsetArray[i][j].x,offsetArray[i][j].y,offsetArray[i][j+1].x,offsetArray[i][j+1].y, offsetArray[i-1][j+1].x, offsetArray[i-1][j+1].y, true);
+        let triangle = two.makePath(offsetArray[i][j].x- AVG_TRI_DIM,offsetArray[i][j].y- AVG_TRI_DIM,offsetArray[i][j+1].x- AVG_TRI_DIM,offsetArray[i][j+1].y- AVG_TRI_DIM, offsetArray[i-1][j+1].x- AVG_TRI_DIM, offsetArray[i-1][j+1].y- AVG_TRI_DIM, true);
         triangle.noStroke().fill = generateGradient(triangle);
     }
 }
